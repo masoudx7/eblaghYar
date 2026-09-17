7e056cd5d2239a19ccdf5b6f119b342d63591af0
masoudx7 <masoudx7@gmail.com>
2026-09-17 11:07:23 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { initBilling } from './services/billing' // 👈 خط جدید ۱: ایمپورت کردن
+
+initBilling()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
